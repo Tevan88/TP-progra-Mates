5f6e11a21b9d3c852ca406039897b01f8e2cbf04
Tevan88 <tevanrivarola@gmail.com>
2025-04-21 14:57:58 -0300

    Simplificando

--- binarioadecimal.py
+++ binarioadecimal.py
@@ -14,12 +14,12 @@ def numero_a_decimal(numero):
             digito = numero % 10
             numero = numero // 10
             suma += (2 ** i) * digito
-            return suma
+        return suma
     else:
       print("El número ingresado no es un número binario válido")
 
 numero = input("Ingrese un numero binario que desee convertir: ")
-print(numero_a_decimal(numero))
-print(f"El número binario {numero} en decimal es {numero_a_decimal(numero)}")  #esto despues podriamos sacarlo y ponerlo en el menu principal del conversor?
+if verificar_binario == True:
+   print(f"El número binario {numero} en decimal es {numero_a_decimal(numero)}")  #esto despues podriamos sacarlo y ponerlo en el menu principal del conversor?
         #pienso que no siempre vamos a querer que esta funcion imprima algo. Por ejemplo cuando la reutilicemos para la suma de binarios. No vamos a querer que diga "este numero es tan en decimal",
         #sino que sume y listo, no?
